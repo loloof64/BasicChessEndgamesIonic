dad3eeacff9d3f50942a331dcce8b460d287db7e
Laurent Bernabe <laurent.bernabe@gmail.com>
2019-07-20 22:36:55 +0200

    Finished coordinates.

--- src/app/loloof64-chessboard/loloof64-chessboard.component.ts
+++ src/app/loloof64-chessboard/loloof64-chessboard.component.ts
@@ -10,6 +10,9 @@ export class Loloof64ChessboardComponent implements OnInit {
   @Input() size = '200px';
   @ViewChild('root') root: ElementRef;
 
+  allFilesCoordinates: string = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  allRanksCoordinates: string = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
@@ -19,6 +22,14 @@ export class Loloof64ChessboardComponent implements OnInit {
 
   cellColorClass(file: number, rank: number): string {
     return (file + rank) % 2 === 0 ? 'white-cell' : 'black-cell';
+  }
+
+  fileCoordinate(file: number): string {
+    return this.allFilesCoordinates[file];
+  }
+
+  rankCoordinate(rank: number): string {
+    return this.allRanksCoordinates[7 - rank];
   }
 
 }
